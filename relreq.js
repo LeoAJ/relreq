d00fcd7229fb4d6eed376db7bf1b161acef17bba
LeoAJ <leo0924@gmail.com>
2014-11-06 15:19:00 -0800

    to support multiple input parameters

--- relreq.js
+++ relreq.js
@@ -2,6 +2,15 @@
 
 var path = require('path');
 
-module.exports = function (relativePath) {
-  return require(path.resolve(relativePath));
+module.exports = function () {
+
+  var key, args = [];
+
+  for (key in arguments) {
+    if (arguments.hasOwnProperty(key)) {
+      args.push(arguments[key]);
+    }
+  }
+
+  return require(path.resolve.apply(null, args));
 };
